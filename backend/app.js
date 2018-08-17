71c43a03141b1b3964da589bf543d4b8eb7a28f9
djovanov92 <djovanov92@gmail.com>
2018-08-17 09:15:42 +0200

    Added connection to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
+const mongoose = require('mongoose');
 
 const app = express();
+//JHTiFABglCj2R4IK
+mongoose.connect("mongodb+srv://dejan:JHTiFABglCj2R4IK@cluster0-csrpr.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed .. !');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,29 +30,35 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: "Post added succefully"
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added succefully!",
+      postId: createdPost._id
+    });
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-    id: "someid123",
-    title: "First server side post",
-    content: "This is coming from the server"
-    },
-    {
-      id: "123someid",
-      title: "Second server side post",
-      content: "This is coming from the server!!"
-      }
-  ]
-  res.status(200).json({
-    message: "post fetched succefully",
-    posts: posts
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: "post fetched succefully",
+        posts: documents
+      });
+    });
+});
+
+app.delete('/api/posts/:id', (req,res,next) => {
+  Post.deleteOne({_id: req.params.id})
+    .then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: "Post deleted!"
+    });
   });
 });
 

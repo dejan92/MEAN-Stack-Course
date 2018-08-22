a43cb55f02b624a304100b1dc72ad545fa6e95c9
djovanov92 <djovanov92@gmail.com>
2018-08-22 15:41:28 +0200

    Enhancing the app

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Post = require('./models/post');
 const mongoose = require('mongoose');
 
+const postsRoutes = require('./routes/posts');
+
 const app = express();
+
 //JHTiFABglCj2R4IK
 mongoose.connect("mongodb+srv://dejan:JHTiFABglCj2R4IK@cluster0-csrpr.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true })
   .then(() => {
@@ -24,42 +26,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added succefully!",
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: "post fetched succefully",
-        posts: documents
-      });
-    });
-});
-
-app.delete('/api/posts/:id', (req,res,next) => {
-  Post.deleteOne({_id: req.params.id})
-    .then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post deleted!"
-    });
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;

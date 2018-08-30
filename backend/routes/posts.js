b8b6d2eb95daed96be6094b0333d02aa077ff106
djovanov92 <djovanov92@gmail.com>
2018-08-30 21:27:45 +0200

    user authentication added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -34,13 +34,16 @@ const storage = multer.diskStorage({
 router.post(
   "",
   checkAuth,
-  multer({ storage: storage }).single("image"),
+  multer({
+    storage: storage
+  }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
     const post = new Post({
       title: req.body.title,
       content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename
+      imagePath: url + "/images/" + req.file.filename,
+      creator: req.userData.userId
     });
     post.save().then(createdPost => {
       res.status(201).json({
@@ -57,7 +60,9 @@ router.post(
 router.put(
   "/:id",
   checkAuth,
-  multer({ storage: storage }).single("image"),
+  multer({
+    storage: storage
+  }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
     if (req.file) {
@@ -68,11 +73,22 @@ router.put(
       _id: req.body.id,
       title: req.body.title,
       content: req.body.content,
-      imagePath: imagePath
+      imagePath: imagePath,
+      creator: req.userData.userId
     });
-    console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      res.status(200).json({ message: "Update successful!" });
+    Post.updateOne({
+      _id: req.params.id,
+      creator: req.userData.userId
+    }, post).then(result => {
+      if (result.nModified > 0) {
+        res.status(200).json({
+          message: "Update successful!"
+        });
+      } else {
+        res.status(401).json({
+          message: "Not authorized!"
+        });
+      }
     });
   }
 );
@@ -104,15 +120,27 @@ router.get("/:id", (req, res, next) => {
     if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found!" });
+      res.status(404).json({
+        message: "Post not found!"
+      });
     }
   });
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
+  Post.deleteOne({
+    _id: req.params.id,
+    creator: req.userData.userId
+  }).then(result => {
+    if (result.n > 0) {
+      res.status(200).json({
+        message: "Deletion successful!"
+      });
+    } else {
+      res.status(401).json({
+        message: "Not authorized!"
+      });
+    }
   });
 });
 
